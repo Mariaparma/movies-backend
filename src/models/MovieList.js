a693d2a9a6c0838c3b08ac0db8b891e2a0f68ce7
maria parma <mariaparma.tds1.senai@gmail.com>
2025-02-06 14:56:30 -0300

    feat: create MovieList.js

---
+++ src/models/MovieList.js
@@ -0,0 +1,40 @@
+class MovieList {
+    constructor() {
+        this.movies = [];
+    }
+    addMovie(movie) {
+        this.movie.push(movie);
+    }
+    getAllMovies(){
+        return this.songs;
+    }
+
+    getMovieById(id){
+        const movie = this.movies.find(movie => movie.id == id);
+        if(!movie){
+            throw new Error("Filme não encontrado");
+        }
+        return movie;
+    }
+
+    updateMovie(id, updateData) {
+        const movie = getMovieById(id);
+        Object.assign(song, updateData);
+        return movie;
+    }
+
+    deleteMovie(id) {
+        this.movies = this.movies.filter(movie => movie.id != id);
+    }
+
+    getTop10Movies() {
+        return this.songs.sort((a,b) => b.movies - a.plays).slice(0, 10);
+    }
+
+    getTotalMovies(){
+        return this.movies.length;
+    }
+
+} 
+  
+module.exports = MovieList;
